0ed1bad0c2a8eb22c6e5e49adf425662d3c6228e
hu9o <hugo.ratiney@gmail.com>
2018-03-29 22:44:18 +0400

    Support anim direction when importing .ase files:
    If the direction of an anim is "reverse", the frames will be reversed on
    animation creation; if it's set to "pingpong", additional frames will be
    added so the anim plays forward then backward.

--- Assets/AnimationImporter/Editor/ImportedData/ImportedAnimation.cs
+++ Assets/AnimationImporter/Editor/ImportedData/ImportedAnimation.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace AnimationImporter
 {
+	public enum PlaybackDirection
+	{
+		Forward, // default
+		Reverse, // reversed frames
+		PingPong // forward, then reverse
+	}
+
 	public class ImportedAnimation
 	{
 		public string name;
@@ -23,6 +32,9 @@ namespace AnimationImporter
 		public int firstSpriteIndex;
 		public int lastSpriteIndex;
 
+		// unity animations only play forward, so this will affect the way frames are added to the final animation clip
+		public PlaybackDirection direction;
+
 		// used with the indices because we to not have the Frame array yet
 		public int Count
 		{
@@ -36,5 +48,24 @@ namespace AnimationImporter
 		//  public methods
 		// --------------------------------------------------------------------------------
 
+		/// <summary>
+		/// Lists frames so that the final anim seems to play in the desired direction.
+		/// *Attention:* Can return more than <see cref="Count"/> frames. 
+		/// </summary>
+		public IEnumerable<ImportedAnimationFrame> ListFramesAccountingForPlaybackDirection()
+		{
+			switch (direction)
+			{
+				default:
+				case PlaybackDirection.Forward: // ex: 1, 2, 3, 4
+					return frames;
+
+				case PlaybackDirection.Reverse: // ex: 4, 3, 2, 1
+					return frames.Reverse();
+
+				case PlaybackDirection.PingPong: // ex: 1, 2, 3, 4, 3, 2
+					return frames.Concat(frames.Skip(1).Take(frames.Length - 2).Reverse());
+			}
+		}
 	}
 }
